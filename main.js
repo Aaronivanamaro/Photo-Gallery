a89704d82038ee29e7a1e992696c775fb1c688b1
Aaronivanamaro <76709054+Aaronivanamaro@users.noreply.github.com>
2021-03-10 22:52:17 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const items = document.querySelectorAll('.item');
+
+function toggleOpen() {
+      this.classList.toggle('open');
+    }
+
+function toggleActive(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+items.forEach(item => item.addEventListener('click', toggleOpen));
+items.forEach(item => item.addEventListener('transitionend', toggleActive));
